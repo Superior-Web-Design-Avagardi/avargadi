9c5d75adf939d8841a73a455719df7df1607e848
jpmaidment <jpmaidment@outlook.com>
2015-05-10 10:53:28 +1000

    Update functions.php
    Removed 'From: ' from the variable product

--- functions.php
+++ functions.php
@@ -51,12 +51,12 @@ add_action('thematic_aboveheader', 'addTopNav');
 function wc_wc20_variation_price_format( $price, $product ) {
 	// Main Price
 	$prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
-	$price = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+	$price = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
 
 	// Sale Price
 	$prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
 	sort( $prices );
-	$saleprice = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+	$saleprice = $prices[0] !== $prices[1] ? sprintf( __( '%1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
 
 	if ( $price !== $saleprice ) {
 		$price = '<del>' . $saleprice . '</del> <ins>' . $price . '</ins>';
